084d3cd5edd55ff1ef27d92a9a8e2fd62cd64470
rakesh baleri <rrbaleri@gmail.com>
2024-11-11 16:05:42 +0530

    javascript

---
+++ DAY1/javascript/map_filter.js
@@ -0,0 +1,17 @@
+const array = [3, 6, 8, 2, 1, 9];
+
+const array1 = array.map((n) => n * n * n); //map
+console.log(array1);
+
+const array3 = array1.filter((f) => f % 2 == 0); //filter
+console.log(array3);
+
+//reducer
+const cart = [
+  { name: "laptop", price: 5000 },
+  { name: "pendrive", price: 500 },
+  { name: "charger", price: 1000 },
+];
+//function with two args accum and item
+const value = cart.reduce((accumulator, item) => accumulator + item.price, 0);
+console.log(value);
