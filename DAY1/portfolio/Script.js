ac96e42ae9919ce9da77c9f1e38a04c9164506fc
rakesh baleri <rrbaleri@gmail.com>
2024-11-11 15:28:18 +0530

    contactus

---
+++ DAY1/portfolio/Script.js
@@ -0,0 +1,7 @@
+function contactus() {
+  let about = document.getElementById("aboutus_div");
+
+  about.innerHTML =
+    "<div>  Address: Behind KDCC Bank Kumta  <br></br>Ph: 9741564277</div>";
+  about.style.color = "white";
+}
--- DAY1/registration_form_basic/Script.js
+++
@@ -1,6 +0,0 @@
-let result = document.getElementsByClassName("result");
-let submit = document.getElementById("submit");
-
-submit.addEventListener("click", () => {
-  result.innerText = "form submitted";
-});
