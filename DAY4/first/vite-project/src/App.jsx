4280f5358e004dcc18ca273c66646f7db2cb2f59
rakesh baleri <rrbaleri@gmail.com>
2024-11-14 10:27:57 +0530

    day 4 first

---
+++ DAY4/first/vite-project/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
+import Nav from "./component/Nav";
+function App() {
+  return (
+    <>
+      <Nav></Nav>
+    </>
+  );
+}
+
+export default App;
