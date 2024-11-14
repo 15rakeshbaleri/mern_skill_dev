f4b5d67bb0b4bcf6358c80c12ab4e7f2db8cf97d
rakesh baleri <rrbaleri@gmail.com>
2024-11-14 11:07:36 +0530

    trainer_db model

--- DAY4/first/vite-project/src/component/Nav.jsx
+++ DAY4/first/vite-project/src/component/Nav.jsx
@@ -4,7 +4,7 @@ function Nav() {
   return (
     <>
       <div className={Style.container}>
-        <nav class="navbar bg-primary">
+        <nav class="navbar navbar-expand-lg bg-primary">
           <div class="container-fluid">
             <a>
               <img
@@ -14,6 +14,30 @@ function Nav() {
                 alt=""
               />
             </a>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">
+                    Home
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">
+                    Features
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">
+                    Pricing
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link disabled" aria-disabled="true">
+                    Disabled
+                  </a>
+                </li>
+              </ul>
+            </div>
             <form class="d-flex" role="search">
               <input
                 class="form-control me-2"
